3fdf99b79155817c808950b60b6e260adeeacc05
Gabriel H. <gabrielhono.o@gmail.com>
2026-04-02 20:43:37 +0200

    component (à tester quand les classes soient écrites)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,46 @@
-import type { ReactNode } from "react";
+import type React from "react";
 
 interface ButtonProps {
-  variant: "primary" | "secondary" | "ghost" | "destructive";
-  size: "m" | "s" | "xs";
-  disabled: boolean;
-  leftIcon?: ReactNode;
-  rightIcon?: ReactNode;
-  badge?: number;
-  iconOnly: boolean;
-  onClick?: () => void;
-  children?: ReactNode;
+    variant: "primary" | "secondary" | "ghost" | "destructive" | "alpha-dark" | "alpha-light";
+    size: "m" | "s" | "xs";
+    disabled: boolean;
+    leftIcon?: React.ReactNode;
+    rightIcon?: React.ReactNode;
+    badge?: number;
+    iconOnly: boolean;
+    onClick: () => void;
+    children?: React.ReactNode;
+}
+
+export default function Button ({
+    variant = "primary",
+    size = "m",
+    disabled = false,
+    leftIcon,
+    rightIcon,
+    badge,
+    iconOnly = false,
+    onClick,
+    children,
+}: ButtonProps) {
+
+    const buttonClass = [
+        "btn",
+        `btn--${variant}`,
+        `btn--${size}`,
+        iconOnly? "btn--icon-only" : ""
+    ].filter(Boolean).join(" ")
+
+    return (
+    <button
+      className={buttonClass}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {leftIcon && <span className="btn_icon">{leftIcon}</span>}
+      {!iconOnly && children}
+      {rightIcon && <span className="btn_icon">{rightIcon}</span>}
+      {badge !== undefined && <span className="btn_badge">{badge}</span>}
+    </button>
+    )
 }
