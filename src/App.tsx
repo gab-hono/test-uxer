967a5b01ec4f3e3cfa5e0691fa4c111f4b34ad14
Gabriel H. <gabrielhono.o@gmail.com>
2026-04-06 14:32:07 +0200

    Correction de typage et test des prémiers boutons. Il resta à faire le "Button Group"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import './App.css'
 import Button from './components/Button/Button'
+import { CheckIcon } from './components/Icons/CheckIcon'
+import { FilterIcon } from './components/Icons/FilterIcon'
 
 function App() {
 
   return (
     <>
+    <ul>
+      <h1>Liste des Boutons</h1>
+      <li>
+        <h2>Bouton primaire avec "Check" icon à gauche</h2>
+        <Button
+          variant='primary'
+          size='m'
+          leadingIcon = {<CheckIcon />}
+          >
+            Valider
+        </Button>   
+      </li>
+
+      <li>
+        <h2>Bouton secondaire avec "filter" icon à gauche et badge avec numéro 3</h2>
+        <Button 
+          variant='secondary'
+          size='m'
+          leadingIcon = {<FilterIcon />}
+          badge={3}>
+            Filter
+          </Button>
+      </li>
+    </ul>
     </>
+
   )
 }
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,19 @@
 import type React from "react";
+import "./Button.css"
 
 interface ButtonProps {
     variant: "primary" | "secondary" | "ghost" | "destructive" | "alpha-dark" | "alpha-light";
     size: "m" | "s" | "xs";
-    disabled: boolean;
+    disabled?: boolean;
     leadingIcon?: React.ReactNode;
     trailingIcon?: React.ReactNode;
     badge?: number;
-    iconOnly: boolean;
-    onClick: () => void;
+    iconOnly?: boolean;
+    onClick?: () => void;
     children?: React.ReactNode;
 }
 
-export default function Button ({
+export default function Button({
     variant = "primary",
     size = "m",
     disabled = false,
@@ -25,22 +26,21 @@ export default function Button ({
 }: ButtonProps) {
 
     const buttonClass = [
-        "btn",
-        `btn--${variant}`,
-        `btn--${size}`,
-        iconOnly? "btn--icon-only" : ""
+        iconOnly ? "btn--icon-only" : "btn",
+        iconOnly ? `btn--icon-only-${variant}` : `btn--${variant}`,
+        !iconOnly && `btn--${size}`,
     ].filter(Boolean).join(" ")
 
     return (
-    <button
-      className={buttonClass}
-      disabled={disabled}
-      onClick={onClick}
-    >
-      {leadingIcon && <span className="leading-icon">{leadingIcon}</span>}
-      {!iconOnly && children}
-      {trailingIcon && <span className="trailing-icon">{trailingIcon}</span>}
-      {badge !== undefined && <span className="badge">{badge}</span>}
-    </button>
+        <button
+            className={buttonClass}
+            disabled={disabled}
+            onClick={onClick}
+        >
+            {leadingIcon && <span className="leading-icon">{leadingIcon}</span>}
+            {!iconOnly && children}
+            {trailingIcon && <span className="trailing-icon">{trailingIcon}</span>}
+            {badge !== undefined && <span className="badge">{badge}</span>}
+        </button>
     )
 }
